1cff52d11479933de163620f2295f6e1c4e19864
cyril <cyril@haberer.fr>
2026-01-25 23:33:16 +0100

    add clock and mistakes

--- front/js/app.js
+++ front/js/app.js
@@ -2,9 +2,13 @@ let mistakestxt = document.getElementById("mistakes");
 let selectedCell;
 let mistakes = 0;
 let difficulty = ["easy","medium","hard"];
+let level = "easy";
+let startTime = null;
+let interval = null;
+let elapsed = 0;
 
-const fetchBoard = async(level) => {
-    const res = await fetch(`http://127.0.0.1:8000/sudoku_grid/${level}`)
+const fetchBoard = async(level_dificulty) => {
+    const res = await fetch(`http://127.0.0.1:8000/sudoku_grid/${level_dificulty}`)
     if (!res.ok) {
         console.error('HTTP error', res.status);
         return;
@@ -29,11 +33,16 @@ const startGame = async(level) => {
     const board = boardGenAndSolution[0];
     const solution = boardGenAndSolution[1];
 
-    console.log(board, solution)
-
+    resetClock();
+    startClock();
+    level = level;
+    mistakes = 0;
+    document.getElementById('level').innerHTML = `level : ${level}`;
+    document.getElementById('mistakes').innerHTML = `mistakes : ${mistakes}`
     document.getElementById('grid').innerHTML = '';
     document.getElementById('selection').innerHTML = '';
     document.getElementById('difficulty').innerHTML = '';
+
 
     for (let i = 0; i <= 9; i+=1) {
         let digit = document.createElement('div');
@@ -90,17 +99,41 @@ const startGame = async(level) => {
                 selectedCell.classList.remove("wrong-digit");
                 selectedCell.classList.remove("correct-digit");
                 selectedCell.innerHTML = '';
-            }
-            const i = selectedCell.id.charAt(0)
-            const j = selectedCell.id.charAt(2)
-            if (solution[i][j].value == this.id){
-                selectedCell.classList.remove("wrong-digit")
-                selectedCell.classList.add("correct-digit")
             } else {
-                selectedCell.classList.remove("correct-digit")
-                selectedCell.classList.add("wrong-digit")
+                const i = selectedCell.id.charAt(0)
+                const j = selectedCell.id.charAt(2)
+                if (solution[i][j].value == this.id){
+                    selectedCell.classList.remove("wrong-digit")
+                    selectedCell.classList.add("correct-digit")
+                    if (checkFinish()) {
+                        endGame();
+                    }
+                } else {
+                    selectedCell.classList.remove("correct-digit")
+                    selectedCell.classList.add("wrong-digit")
+                    mistakes += 1
+                    document.getElementById('mistakes').innerHTML = `mistakes : ${mistakes}`
+                }
             }
         }
+    }
+
+    function checkFinish() {
+        const cells = document.querySelectorAll("#grid div");
+        let allValid = true;
+        cells.forEach(cell => {
+        if (
+            !cell.classList.contains("correct-digit") &&
+            !cell.classList.contains("fixed-cell")
+        ) {
+            allValid = false;
+        }
+        });
+        return allValid;
+    }
+
+    function endGame() {
+        stopClock();
     }
 
     async function displayDifficulty() {
@@ -109,7 +142,33 @@ const startGame = async(level) => {
         document.getElementById('difficulty').style.visibility = 'hidden';
     }
 
+    async function startClock() {
+        if (interval) return;
+        startTime = Date.now() - (elapsed || 0);
+        interval = setInterval(update, 100);
+    }
 
+    async function update() {
+        elapsed = Date.now() - startTime;
+        let totalSeconds = Math.floor(elapsed / 1000);
+
+        let h = Math.floor(totalSeconds / 3600);
+        let m = Math.floor((totalSeconds % 3600) / 60);
+        let s = totalSeconds % 60;
+
+        document.getElementById("clock").innerHTML =
+            `clock : ${h.toString().padStart(2,"0")}:${m.toString().padStart(2,"0")}:${s.toString().padStart(2,"0")}`;
+    }
+
+    async function stopClock() {
+        clearInterval(interval);
+        interval = null;
+    }
+
+    async function resetClock() {
+        stopClock();
+        elapsed = 0;
+    }
 }
 
-startGame('easy');
+startGame(level);
